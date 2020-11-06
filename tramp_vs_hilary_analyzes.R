32d3b3b3fa64c815f10ce08ba361540bb0bad032
max <maksim.kalganov@gmail.com>
2020-11-06 17:19:49 +0300

    finish analyzes for hillary vs trump data

--- tramp_vs_hilary_analyzes.R
+++ tramp_vs_hilary_analyzes.R
@@ -8,8 +8,9 @@ for(file in list.files(folder)){
     cur_df <- read.csv(paste(folder, file, sep="/"), header = TRUE)
     state <- word(file, sep="_County")
     cur_df$state <- state
-    cur_df <- cur_df[, c("Candidate.Name", "state", "Total.Votes")]
-    colnames(cur_df) <- c("name", "state", "votes")
+    cur_df <- cur_df[, c("Candidate.Name", "state", "Total.Votes", "Winner")]
+    colnames(cur_df) <- c("name", "state", "votes", "winner")
+    cur_df$winner <- as.character(cur_df$winner)
     if (typeof(full_df) == "character"){
       full_df <- cur_df
     }else{
@@ -18,10 +19,10 @@ for(file in list.files(folder)){
   }
 }
 
-tramp_name <- "Donald J. Trump"
+trump_name <- "Donald J. Trump"
 hillary_name <- "Hillary Clinton"
 
-only_main_candidates <- subset(full_df, (name == tramp_name | name == hillary_name))
+only_main_candidates <- subset(full_df, (name == trump_name | name == hillary_name))
 
 ggplot(only_main_candidates, aes(x="", y=votes, fill=state)) +
   scale_fill_viridis_d() + 
@@ -31,7 +32,7 @@ ggplot(only_main_candidates, aes(x="", y=votes, fill=state)) +
   theme_minimal()
 
 
-others <- subset(full_df, name != tramp_name & name != hillary_name)
+others <- subset(full_df, name != trump_name & name != hillary_name)
 others$name <- "other"
 main_and_others <- rbind(only_main_candidates, others)
 
@@ -46,7 +47,31 @@ ggplot(grouped_totals, aes(x="", y=votes, fill=name)) +
   coord_polar("y", start=0) +
   theme_minimal()
 
+ggplot(only_main_candidates, aes(x=state, y=votes, fill=name)) + 
+  geom_bar(stat = "identity", position="dodge") + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  ylab("total votes")
+
+# Для одного штата, где победил Трамп
+# Ищу штат, где победил трамп
+trump_won <- subset(full_df, winner == "Y" & name == trump_name)
+trump_won # по данным из файлов ..._Country... нет такого штата, где победил Трамп
+
+hillary_won <- subset(full_df, winner == "Y" & name == hillary_name)
+hillary_won # в то время как Хилари победила в 24 штатах
 
 
+# например в Carroll
+carroll_results <- subset(main_and_others, state=="Carroll")
 
+carroll_grouped_totals <- carroll_results %>% 
+  group_by(name) %>% 
+  summarise(votes = sum(votes))
 
+ggplot(carroll_grouped_totals, aes(x="", y=votes, fill=name)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0) +
+  theme_minimal()
+
+# хоть и total votes там больше у Трампа
+# p.s. данные вообще достаточно странные
